e565b25df8b639f65631dfad488496328a1bffa8
BiolessInteractive <89420429+BiolessInteractive@users.noreply.github.com>
2021-12-14 18:36:29 -0800

    Add files via upload

--- myGame/source/scenes.py
+++ myGame/source/scenes.py
@@ -3,4 +3,4 @@ from ursina import * #Import the game engine
 class scene1(Entity):
 	def __init__(self, **kwargs):
 		super().__init__()
-		self.ground = Entity(model='plane', texture='white_cube', scale=(100,100,100), texture_scale=(100,100,100), collider='mesh')
+		self.ground = Entity(model='plane', texture='assets/textures/prototypeMaterial', scale=(100,100,100), texture_scale=(100,100,100), collider='mesh')
